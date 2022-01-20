b295d4d8370801fda0fbc9bd0db9ecef0f7c6bb7
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2022-01-19 20:44:57 -0500

    initial code for get routes to all notes

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const PORT = 3001;
+const fs = require('fs');
+const path = require('path');
 const allNotes = require('./db/db.json');
+
+const PORT = 3001;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
 // GET route to get all of the notes
-app.get('/', (req, res) => res.json(allNotes));
+app.get('/api/notes', (req, res) => {
+  res.json(allNotes.slice(1))
+});
 
 // Listen for connections
 app.listen(PORT, () =>
   console.info(`Example app listening at http://localhost:${PORT} 🚀`)
 );
+
