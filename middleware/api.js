885cebc83490771c2fa78f6a46e32ee3ae7d8d35
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2022-01-22 16:47:22 -0500

    got data to delete from db.json file, but it is not deleting from webpage view - WIP

--- middleware/api.js
+++ middleware/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const router = express.Router();
+const notes = require('express').Router();
 const fs = require('fs');
+const reload = require('reload');
 
 let data = require('../db/db.json');
 
@@ -10,23 +12,40 @@ router.get('/', (req, res) => res.json(data));
 router.post('/', (req, res) => {
     const newTitle = req.body.title;
     const newText = req.body.text;
+    const note1 = req.body;
+    note1.id = uuidv4();
+
     if (!newTitle || !newText) {
         res.status(400).json({msg: 'Need title and text input.'})
     } else {
-        data.push(req.body);
+        data.push(note1);
         fs.writeFileSync('./db/db.json', JSON.stringify(data), 'utf8');
         res.json(true);
     };
 });
 
-router.delete('/:id', (req, res) => {
-    const found = data.some(obj => obj.id === req.params.id);
-    if (found) {
-        data = data.filter(obj => obj.id !== req.params.id);
-        res.json(data);
-    } else {
-        res.status(400).json(data);
-    };
-});
+router.delete("/:id", (req, res) => {
+    fs.readFile("db/db.json", (err, data) => {
+      // Check for error
+      if (err) throw err;
+      let deleteId = req.params.id;
+      console.log(deleteId);
+      // Handle data gathering for json update
+      let notes = JSON.parse(data);
+      notes.forEach((item, i) => {
+        if (item.id == deleteId) {
+          console.log(item);
+          notes.splice(i, 1);
+        }
+      });
+  
+      // Write updated json to array
+      fs.writeFile("db/db.json", JSON.stringify(notes, null, 2), (err) => {
+        // Check for error
+        if (err) throw err;
+        res.send("200");
+      });
+    });
+  });
 
 module.exports = router;
